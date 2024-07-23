331f0cb393448fa26ffd394f20e78d96ec9d9233
agustina <agus.es.17@gmail.com>
2024-07-22 21:34:58 -0300

    carrousel funcionando

--- public/js/landing.js
+++ public/js/landing.js
@@ -12,3 +12,17 @@ cerrarMenu.addEventListener("click", ()=>{
     menu.classList.remove("nav__hamburger--visible");
     abrirMenu.classList.remove("nav__hamburger--invisible");
 })
+
+//SECCION INTERCAMBIA: Carrousel
+const swap__cards = document.querySelector(".swap__card--container"); 
+const swap__prevBtn = document.querySelector("#swap__prevBtn");
+const swap__nextBtn = document.querySelector("#swap__nextBtn");
+
+
+    swap__nextBtn.addEventListener("click", () => {
+        swap__cards.scrollLeft += 800;
+    });
+    swap__prevBtn.addEventListener("click", () => {
+        swap__cards.scrollLeft -= 800;
+    });
+
