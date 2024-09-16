77091a978a8c08a621f409f923361e8c31927bb5
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-09-16 08:41:52 -0300

    Lectura del query param para obtener los datos de la prenda implementada

--- public/js/detalleprenda.js
+++ public/js/detalleprenda.js
@@ -1,76 +1,73 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const apiUrl = 'https://microservicio-prendas.vercel.app/api/clothes/66e77f42024fba890c6bc232'; 
-    const urlParams = new URLSearchParams(window.location.search);
-    const prendaId = urlParams.get('id');
-    fetch(apiUrl)
-    .then(response => response.json())
-    .then(prenda => {
-        console.log(prenda)
-        document.getElementById('prenda-imagen').src = prenda.media[0].url;
-        document.getElementById('prenda-nombre').textContent = prenda.name;
-        // document.getElementById('prenda-lugar').textContent = prenda.lugar;
-        document.getElementById('prenda-color').textContent = prenda.color;
-        document.getElementById('prenda-talle').textContent = prenda.size;
-        document.getElementById('prenda-categoria').textContent = prenda.category;
-        document.getElementById('prenda-descripcion').textContent = prenda.description;
-        
-
-        document.getElementById('intercambio-color').textContent = prenda.expectedColor;
-        document.getElementById('intercambio-talle').textContent = prenda.expectedSize;
-        document.getElementById('intercambio-categoria').textContent = prenda.expectedCategory;
-
-        cargarPrendasRelacionadas(prenda.category);
-    })
-    .catch(error => console.error('Error al obtener los detalles de la prenda:', error));
-    // if (prendaId) {
-    //     fetch(`http://localhost:3001/api/prendas/${prendaId}`)
-    //         .then(response => response.json())
-    //         .then(prenda => {
-    //             document.getElementById('nombrePrenda').textContent = prenda.name;
-    //             document.getElementById('ubicacionPrenda').textContent = prenda.lugar;
-    //             document.getElementById('colorPrenda').textContent = prenda.color;
-    //             document.getElementById('tallaPrenda').textContent = prenda.size;
-    //             document.getElementById('categoriaPrenda').textContent = prenda.category;
-    //             document.getElementById('descripcionPrenda').textContent = prenda.description;
-                
-
-    //             document.getElementById('intercambio-color').textContent = prenda.intercambio.color;
-    //             document.getElementById('intercambio-talle').textContent = prenda.intercambio.size;
-    //             document.getElementById('intercambio-categoria').textContent = prenda.intercambio.category;
-
-    //             cargarPrendasRelacionadas(prenda.category);
-    //         })
-    //         .catch(error => console.error('Error al obtener los detalles de la prenda:', error));
-    // } else {
-    //     console.error('No se encontró el ID de la prenda en la URL.');
-    // }
-
-
-    function cargarPrendasRelacionadas(category) {
-        fetch(`${apiUrl}?categoria=${category}`)
-            .then(response => response.json())
-            .then(prendas => {
-                const carrusel = document.getElementById('carrusel-items');
-                carrusel.innerHTML = ''; 
-
-                prendas.forEach(prenda => {
-                    const itemDiv = document.createElement('div');
-                    itemDiv.classList.add('carrusel-item');
-
-                    itemDiv.innerHTML = `
-                        <img src="${prenda.media[0].url}" alt="${prenda.name}">
-                        <h3>${prenda.name}</h3>
-                        <button onclick="irADetalle('${prenda._id}')">Intercambiar</button>
-                    `;
-
-                    carrusel.appendChild(itemDiv);
-                });
-            })
-            .catch(error => console.error('Error al cargar prendas relacionadas:', error));
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    const url = handleUrl();
+    renderCard(url);
 });
 
+async function renderCard(url) {
+    const response = await fetch(url);
+    if (!response) {
+        throw new Error('Network response was not ok');
+
+    }
+    const clothe = await response.json()
+    let userInfo;
+    try {
+        const response = await fetch(`https://microservicio-usuarios-three.vercel.app/api/users/${clothe.userId}`)
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        userInfo = await response.json();
+    }
+    catch (error) {
+        console.error('Error al cargar informacion del usuario: ', error);
+    }
+    document.getElementById('prenda-imagen').src = clothe.media[0].url;
+    document.getElementById('prenda-nombre').textContent = clothe.name;
+    document.getElementById('prenda-lugar').textContent = userInfo.city;
+    document.getElementById('prenda-color').textContent = clothe.color;
+    document.getElementById('prenda-talle').textContent = clothe.size;
+    document.getElementById('prenda-categoria').textContent = clothe.category;
+    document.getElementById('prenda-descripcion').textContent = clothe.description;
+    document.getElementById('intercambio-color').textContent = clothe.expectedColor;
+    document.getElementById('intercambio-talle').textContent = clothe.expectedSize;
+    document.getElementById('intercambio-categoria').textContent = clothe.expectedCategory;
+}
 
 function irADetalle(prendaId) {
     window.location.href = `detalle.html?id=${prendaId}`;
+}
+
+function getQueryParams() {
+    const currentUrl = window.location.href;
+    const url = new URL(currentUrl);
+    const params = new URLSearchParams(url.search);
+    return Object.fromEntries(params.entries());
+}
+function handleUrl() {
+    let baseClotheUrl = 'https://microservicio-prendas.vercel.app/api/clothes';
+    const { id } = getQueryParams();
+    baseClotheUrl += `/${id}`;
+    return baseClotheUrl;
+}
+function cargarPrendasRelacionadas(category) {
+    fetch(`${apiUrl}?categoria=${category}`)
+        .then(response => response.json())
+        .then(prendas => {
+            const carrusel = document.getElementById('carrusel-items');
+            carrusel.innerHTML = '';
+
+            prendas.forEach(prenda => {
+                const itemDiv = document.createElement('div');
+                itemDiv.classList.add('carrusel-item');
+
+                itemDiv.innerHTML = `
+                    <img src="${prenda.media[0].url}" alt="${prenda.name}">
+                    <h3>${prenda.name}</h3>
+                    <button onclick="irADetalle('${prenda._id}')">Intercambiar</button>
+                `;
+
+                carrusel.appendChild(itemDiv);
+            });
+        })
+        .catch(error => console.error('Error al cargar prendas relacionadas:', error));
 }
