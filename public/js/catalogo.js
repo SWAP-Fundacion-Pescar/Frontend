b080861d723591ada8ee304b59131e26453f7a00
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-09-15 23:04:12 -0300

    Modificacion de URL's

--- public/js/catalogo.js
+++ public/js/catalogo.js
@@ -1,33 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
     const catalogoDiv = document.getElementById('catalogo');
 
-    const apiUrl = 'http://localhost:3005/Microservicio_Prendas';
+    const clotheUrl = 'https://microservicio-prendas.vercel.app/api/clothes';   
 
     // obtener y mostrar las prendas
     async function cargarPrendas() {
         try {
-            const response = await fetch(apiUrl);
+            const response = await fetch(clotheUrl);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             const prendas = await response.json();
-
             // limpia del catálogo
             catalogoDiv.innerHTML = '';
 
             // iterar  y agregarlas al DOM
-            prendas.forEach(prenda => {
+            prendas.forEach(async prenda => {
                 const prendaDiv = document.createElement('div');
                 prendaDiv.className = 'prenda';
+                let userInfo;
+                try
+                {
+                    const response = await fetch(`https://microservicio-usuarios-three.vercel.app/api/users/${prenda.userId}`)
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    userInfo = await response.json();
+                }
+                catch(error)
+                {
+                    console.error('Error al cargar informacion del usuario: ', error);
+                }
+                console.log(userInfo)
                 prendaDiv.innerHTML = `
-                    <img src="${prenda.imagen}" alt="${prenda.nombre}" class="prenda__img">
-                    <h3>${prenda.nombre}</h3>
-                    <p>${prenda.descripcion}</p>
-                    <p>Precio: ${prenda.precio}</p>
-                    <div class="actions">
-                        <button class="favorite"><i class="fa fa-heart"></i></button>
-                        <button class="exchange">Intercambiar</button>
-                    </div>
+                    <div class="prenda-img"><img src="${prenda.media[0].url}" alt="${prenda.name}" class="prenda__img"></div>
+                    <p>${prenda.name}</p>
+                    <p>${userInfo.city}</p>                                            
+                    <a href="" class="swap__card__button button">Intercambiar</a>                 
                 `;
                 catalogoDiv.appendChild(prendaDiv);
             });
--- public/js/detalleprenda.js
+++ public/js/detalleprenda.js
@@ -1,30 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const apiUrl = 'http://localhost/api/Clothes'; 
+    const apiUrl = 'https://microservicio-prendas.vercel.app/api/clothes/66e77f42024fba890c6bc232'; 
     const urlParams = new URLSearchParams(window.location.search);
     const prendaId = urlParams.get('id');
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(prenda => {
+        console.log(prenda)
+        document.getElementById('prenda-imagen').src = prenda.media[0].url;
+        document.getElementById('prenda-nombre').textContent = prenda.name;
+        // document.getElementById('prenda-lugar').textContent = prenda.lugar;
+        document.getElementById('prenda-color').textContent = prenda.color;
+        document.getElementById('prenda-talle').textContent = prenda.size;
+        document.getElementById('prenda-categoria').textContent = prenda.category;
+        document.getElementById('prenda-descripcion').textContent = prenda.description;
+        
 
-    if (prendaId) {
-        fetch(`http://localhost:3001/api/prendas/${prendaId}`)
-            .then(response => response.json())
-            .then(prenda => {
-                document.getElementById('nombrePrenda').textContent = prenda.name;
-                document.getElementById('ubicacionPrenda').textContent = prenda.lugar;
-                document.getElementById('colorPrenda').textContent = prenda.color;
-                document.getElementById('tallaPrenda').textContent = prenda.size;
-                document.getElementById('categoriaPrenda').textContent = prenda.category;
-                document.getElementById('descripcionPrenda').textContent = prenda.description;
+        document.getElementById('intercambio-color').textContent = prenda.expectedColor;
+        document.getElementById('intercambio-talle').textContent = prenda.expectedSize;
+        document.getElementById('intercambio-categoria').textContent = prenda.expectedCategory;
+
+        cargarPrendasRelacionadas(prenda.category);
+    })
+    .catch(error => console.error('Error al obtener los detalles de la prenda:', error));
+    // if (prendaId) {
+    //     fetch(`http://localhost:3001/api/prendas/${prendaId}`)
+    //         .then(response => response.json())
+    //         .then(prenda => {
+    //             document.getElementById('nombrePrenda').textContent = prenda.name;
+    //             document.getElementById('ubicacionPrenda').textContent = prenda.lugar;
+    //             document.getElementById('colorPrenda').textContent = prenda.color;
+    //             document.getElementById('tallaPrenda').textContent = prenda.size;
+    //             document.getElementById('categoriaPrenda').textContent = prenda.category;
+    //             document.getElementById('descripcionPrenda').textContent = prenda.description;
                 
 
-                document.getElementById('intercambio-color').textContent = prenda.intercambio.color;
-                document.getElementById('intercambio-talle').textContent = prenda.intercambio.size;
-                document.getElementById('intercambio-categoria').textContent = prenda.intercambio.category;
+    //             document.getElementById('intercambio-color').textContent = prenda.intercambio.color;
+    //             document.getElementById('intercambio-talle').textContent = prenda.intercambio.size;
+    //             document.getElementById('intercambio-categoria').textContent = prenda.intercambio.category;
 
-                cargarPrendasRelacionadas(prenda.category);
-            })
-            .catch(error => console.error('Error al obtener los detalles de la prenda:', error));
-    } else {
-        console.error('No se encontró el ID de la prenda en la URL.');
-    }
+    //             cargarPrendasRelacionadas(prenda.category);
+    //         })
+    //         .catch(error => console.error('Error al obtener los detalles de la prenda:', error));
+    // } else {
+    //     console.error('No se encontró el ID de la prenda en la URL.');
+    // }
 
 
     function cargarPrendasRelacionadas(category) {
