f5e375b7fe0238e0695c51816056dac9e8f50d7c
agustina <agus.es.17@gmail.com>
2024-09-20 14:57:36 -0300

    Detalle Prenda Intercambio: pagina nueva + botones conectados

--- public/js/detalleprenda.js
+++ public/js/detalleprenda.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 async function renderCard(url) {
     const response = await fetch(url);
-    if (!response) {
+    if (!response.ok) {
         throw new Error('Network response was not ok');
 
     }
