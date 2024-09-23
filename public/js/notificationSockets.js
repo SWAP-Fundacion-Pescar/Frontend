16a978262a6df9d377bcd717a4dc493da45d70d3
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-09-22 21:16:25 -0300

    Socket de notificaciones implementados en la landing

--- public/js/notificationSockets.js
+++ public/js/notificationSockets.js
@@ -1,5 +1,5 @@
 import { getCookie } from "./helpers.js";
-const socketUrl = 'http://localhost:3004';
+const socketUrl = 'https://microservicio-notificaciones.onrender.com';
 const notificationSocket = io(socketUrl, {
     extraHeaders: {
         authorization: `bearer ${getCookie('token')}`
@@ -7,9 +7,11 @@ const notificationSocket = io(socketUrl, {
 });
 document.addEventListener('DOMContentLoaded', () => {
     const userId = getCookie('ID');
-    notificationSocket.emit('join', userId)
-    notificationSocket.emit('getNotificationsChat', userId);
-    notificationSocket.emit('getNotificationsExchange', userId);
+    if (userId) {
+        notificationSocket.emit('join', userId)
+        notificationSocket.emit('getNotificationsChat', userId);
+        notificationSocket.emit('getNotificationsExchange', userId);
+    }
 })
 notificationSocket.on('connect', () => {
     console.log('Connected to the Socket.IO server.');
@@ -18,11 +20,10 @@ notificationSocket.on('notificationsChat', (content) => {
     //TODO Actualizar icono de chats en base a la cantidad de mensajes y rellenar un div invisible con el contenido
     console.log(content)
 })
-notificationSocket.on('notificationsExchange', (content) => 
-    {
-        //TODO Actualizar icono de notificaciones en base a la cantidad y rellenar un div invisible con el contenido
-        console.log(content);
-    })
+notificationSocket.on('notificationsExchange', (content) => {
+    //TODO Actualizar icono de notificaciones en base a la cantidad y rellenar un div invisible con el contenido
+    console.log(content);
+})
 export {
     notificationSocket
 }
